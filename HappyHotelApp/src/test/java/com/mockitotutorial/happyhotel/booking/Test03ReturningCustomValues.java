edb2b24b80c0b32008237a36b492aefef35f3811
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-08-22 10:55:59 +0800

    Add Test03ReturningCustomValues class for testing available place count

---
+++ HappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test03ReturningCustomValues.java
@@ -0,0 +1,61 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test03ReturningCustomValues {
+
+    private BookingService bookingService;
+
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setup() {
+        paymentServiceMock = mock(PaymentService.class);
+        roomServiceMock = mock(RoomService.class);
+        bookingDAOMock = mock(BookingDAO.class);
+        mailSenderMock = mock(MailSender.class);
+
+        bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+    }
+
+    @Test
+    void should_CountAvailablePlaces_When_OneRoomAvailable() {
+        // given
+        when(roomServiceMock.getAvailableRooms()).thenReturn(Collections.singletonList(new Room("Room 1", 5)));
+
+        int expected = 5;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void should_CountAvailblePlaces_When_MultipleRoomsAvailable() {
+        // given
+        List<Room> rooms = Arrays.asList(new Room("Room 1", 2), new Room("Room 2", 5));
+        when(roomServiceMock.getAvailableRooms()).thenReturn(rooms);
+
+        int expected = 7;
+
+        // when
+        int actual = bookingService.getAvailablePlaceCount();
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
