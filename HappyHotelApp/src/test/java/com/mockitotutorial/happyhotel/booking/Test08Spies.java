6def90c306397ec4058d1ba8f70f2d78f4ca11c5
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-08-22 17:20:11 +0800

    Add Test08Spies class for testing booking service with spies

---
+++ HappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test08Spies.java
@@ -0,0 +1,59 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Test08Spies {
+
+    private BookingService bookingService;
+
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    @BeforeEach
+    void setup() {
+        paymentServiceMock = mock(PaymentService.class);
+        roomServiceMock = mock(RoomService.class);
+        bookingDAOMock = spy(BookingDAO.class);
+        mailSenderMock = mock(MailSender.class);
+
+        bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+    }
+
+    @Test
+    void should_MakeBooking_When_InputOK() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05),
+                2, true);
+
+        // when
+        String bookingId = bookingService.makeBooking(bookingRequest);
+
+        // then
+        verify(bookingDAOMock).save(bookingRequest);
+        System.out.println("bookingId = " + bookingId);;
+    }
+
+    @Test
+    void should_CancelBooking_When_InputOK() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05),
+                2, true);
+        bookingRequest.setRoomId("1.3");
+        String bookingId = "1";
+
+        doReturn(bookingRequest).when(bookingDAOMock).get(bookingId);
+
+        // when
+        bookingService.cancelBooking(bookingId);  
+    }
+}
