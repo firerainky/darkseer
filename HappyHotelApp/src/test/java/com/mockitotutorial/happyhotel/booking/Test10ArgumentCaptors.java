3db401ada2a276664bad48889bb80d4bf668d4cc
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-08-28 18:42:23 +0800

    Add Test10ArgumentCaptors for capturing method arguments

---
+++ HappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test10ArgumentCaptors.java
@@ -0,0 +1,73 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+public class Test10ArgumentCaptors {
+
+    private BookingService bookingService;
+
+    private PaymentService paymentServiceMock;
+    private RoomService roomServiceMock;
+    private BookingDAO bookingDAOMock;
+    private MailSender mailSenderMock;
+
+    private ArgumentCaptor<Double> doubleCaptor;
+
+    @BeforeEach
+    void setup() {
+        paymentServiceMock = mock(PaymentService.class);
+        roomServiceMock = mock(RoomService.class);
+        bookingDAOMock = mock(BookingDAO.class);
+        mailSenderMock = mock(MailSender.class);
+
+        bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
+
+        doubleCaptor = ArgumentCaptor.forClass(Double.class); 
+    }
+
+    @Test
+    void should_PayCorrectPrice_When_InputOK() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05),
+                2, true);
+
+        // when
+        bookingService.makeBooking(bookingRequest);
+
+        // then
+        verify(paymentServiceMock, times(1)).pay(eq(bookingRequest), doubleCaptor.capture());
+        double capturedArgument = doubleCaptor.getValue();
+        assertEquals(400.0, capturedArgument);;
+    }
+
+    @Test
+    void should_PayCorrectPrice_When_MultipleCalls() {
+        // given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05),
+                2, true);
+        BookingRequest bookingRequest2 = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 02),
+                2, true);
+        List<Double> expectedValues = List.of(400.0, 100.0);
+
+        // when
+        bookingService.makeBooking(bookingRequest);
+        bookingService.makeBooking(bookingRequest2);
+
+        // then
+        verify(paymentServiceMock, times(2)).pay(any(), doubleCaptor.capture());
+        List<Double> capturedArguments = doubleCaptor.getAllValues();
+        assertEquals(expectedValues, capturedArguments);
+    }
+}
