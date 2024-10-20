c9ea73c8deba2229ad1b828a2217b9110cd120b7
zhengkanyan <zhengkanyan@gmail.com>
2024-10-20 22:43:06 +0800

    Added some async operations in SpringBoot applications

--- application/src/main/java/com/zky/application/Application.java
+++ application/src/main/java/com/zky/application/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.concurrent.CompletableFuture;
 
 import com.zky.library.MyService;
 
@@ -20,6 +21,37 @@ public class Application {
 
 	@GetMapping("/")
 	public String home() {
+		return myService.message();
+	}
+
+	@GetMapping("/async")
+	public String asyncButStillSyncing() {
+		CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new IllegalStateException(e);
+			}
+			return myService.message();
+		}).exceptionally(ex -> {
+			return "error";
+		});
+		return future.join();
+	}
+
+	@GetMapping("/asyncPrint")
+	public String asyncPrint() {
+		CompletableFuture.runAsync(() -> {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				throw new IllegalStateException(e);
+			}
+			System.out.println(myService.message());
+		}).exceptionally(ex -> {
+			System.out.println("exception: " + ex);
+			return null;
+		});
 		return myService.message();
 	}
 
