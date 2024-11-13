bc5e1eea3176f7a499d96feab8614526df1b6f64
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-11-13 10:36:02 +0800

    Added mockbean for test purpose

--- application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
+++ application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
@@ -1,20 +1,30 @@
 package com.zky.application.chocolate;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = {ChocolateService.class, ChocolateRepository.class})
+@ContextConfiguration(classes = {ChocolateService.class})
 public class ChocolateServiceTest {
 
     @Autowired
     private ChocolateService chocolateService;
 
+    @MockBean
+    private ChocolateRepository chocolateRepository;
+
     @Test
     void test_getChocolate() {
+        Chocolate expected = new Chocolate(2, "awesome chocolate");
+        when(chocolateRepository.createChocolate()).thenReturn(expected);
+
         Chocolate chocolate = chocolateService.getChocolate();
-        System.out.println(chocolate);
+        assertEquals(expected, chocolate);
     }
 }
