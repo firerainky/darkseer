6bcaf07fc556b2c18228fcd13a1ebe574fb8aa10
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-11-26 11:13:44 +0800

    Upgrade to Spring Boot 3.4.0 and add dependency for MyBatis latest version

--- application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
+++ application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
@@ -6,7 +6,7 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
@@ -16,7 +16,7 @@ public class ChocolateServiceTest {
     @Autowired
     private ChocolateService chocolateService;
 
-    @MockBean
+    @MockitoBean
     private ChocolateRepository chocolateRepository;
 
     @Test
