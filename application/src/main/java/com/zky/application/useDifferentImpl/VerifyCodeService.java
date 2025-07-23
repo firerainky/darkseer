ba23ddec72456649a123aeff9bdf83ce44919519
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-07-23 16:32:42 +0800

    Remove unused useDifferentImpl code

--- application/src/main/java/com/zky/application/useDifferentImpl/VerifyCodeService.java
+++
@@ -1,7 +0,0 @@
-package com.zky.application.useDifferentImpl;
-
-public interface VerifyCodeService {
-
-    String sendVerifyCode(String phoneNumber);
-    boolean verifyCode(String phoneNumber, String code);
-}
