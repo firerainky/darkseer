26952c6b924646a54079e473ebade0f8a712e1a6
zhengkanyan <zhengkanyan@zhejianglab.com>
2025-07-24 15:42:59 +0800

    Add test for printing all ocnfigured beans

--- application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
+++ application/src/test/java/com/zky/application/chocolate/ChocolateServiceTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ContextConfiguration;
 
@@ -14,10 +15,23 @@ import org.springframework.test.context.ContextConfiguration;
 public class ChocolateServiceTest {
 
     @Autowired
+    private ApplicationContext context;
+
+    @Autowired
     private ChocolateService chocolateService;
 
     @MockitoBean
     private ChocolateRepository chocolateRepository;
+
+    @Test
+    void test_printAllBeans() {
+        String[] beanNames = context.getBeanDefinitionNames();
+        System.out.println("=== " + beanNames.length + " Beans in ApplicationContext ===");
+        for (String name : beanNames) {
+            Object bean = context.getBean(name);
+            System.out.println(name + " -> " + bean.getClass().getName());
+        }
+    }
 
     @Test
     void test_getChocolate() {
